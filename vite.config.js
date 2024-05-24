4b8de8be623b3c294397a1538e9a8593f0aebde6
VinolNoronha <vinolnoronha7@gmail.com>
2024-05-25 00:03:48 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,10 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/Restaurant-Order-App/",
   plugins: [react()],
-  base: "/Restaurant Order App/",
+  server: {
+    port: 3000, // Customize the dev server port if needed
+  },
+  // Additional configuration options can go here
 });
