50dfbc6094d0e65da76cf280b652cfa6c478b554
VinolNoronha <vinolnoronha7@gmail.com>
2024-05-25 22:18:37 +0530

    set base path for github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 3000, // Customize the dev server port if needed
-  },
-  // Additional configuration options can go here
+  base: "/Restaurant-Order-App",
 });
